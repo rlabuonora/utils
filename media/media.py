a15d500e7fc94247a4c758aa0b0eb4ee9d393ac1
rafa <rlabuonora@yahoo.com>
2017-12-26 16:51:29 -0300

    Add test stubs

--- media/media.py
+++ media/media.py
@@ -1,18 +1,30 @@
+import sys
+import os
+import pipes
+import glob
+
+
 def get_dest_files(new_extension, new_location, original_files):
     # retrun a list of new files based on the original files
-    pass
+    # new_names = [ os.path.splitext(os.path.basename(video_file))[0] for video_file in original_files]
+    # return new_names
+    return True
 
-def get_original_files(extensions = ["wmv", "avi"]):
+def get_original_files(source_location):
     # returns a list of files to be ripped or converted
-    pass
+    return glob.glob(os.path.join(source_location, '*.avi'))
+    
 
 def run_commands(commands):
     # quotes and runs a list of commands
-    pass
+    [os.system(pipes.quote(command)) for command in commands]
 
 
-def interpolate_command(command, new_file, old_file):
+def interpolate_command(command, old_file, new_file):
     # returns a str with the command to be run
+    ffmpeg_command = substitute_filenames(command, old_file, new_full)
+    return ffmpeg_command
+
 
 def rip_audio(source, destination):
     pass
--- test/test_media.py
+++ test/test_media.py
@@ -1,1 +1,14 @@
 import unittest
+
+from media.media import *
+
+# file_list = ['../files/bird1.avi', '../files/bird1   (copy).avi', '../files/bird.avi']
+
+class TestMedia(unittest.TestCase):
+
+    def test_true(self):
+        self.assertTrue(get_dest_files("", "", ""))
+
+        
+if __name__ == '__main__':
+    unittest.main()
